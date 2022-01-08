74c52b4cbd99a266df750e0fd373a13dfc2b75be
Mayowa Emmanuel <odudareemmanuel@gmail.com>
2022-01-08 10:57:48 +0100

    after setup

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('home');
+        return redirect()->route('login');
 
     }
 }
